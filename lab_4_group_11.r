e05574d88e3f354bf01ec1bfd9a86af775d08ab0
seladore <mbdesai@umich.edu>
2017-04-11 22:55:19 -0400

    improved summaries and explanations

--- lab_4_group_11.r
+++ lab_4_group_11.r
@@ -15,8 +15,8 @@ test_inds = (1:n)[-train_inds]
 pred_results = function(model,
                         train = OJ[train_inds,],
                         test = OJ[test_inds,],
-                        train.y=OJ[train_inds, "Purchase"],
-                        test.y=OJ[test_inds, "Purchase"]){
+                        train.y = OJ[train_inds, "Purchase"],
+                        test.y = OJ[test_inds, "Purchase"]){
   train.hat = predict(model, train)
   test.hat = predict(model, test)
   print(paste("Train Error Rate", sum(train.hat != train.y)/n))
@@ -29,7 +29,7 @@ pred_results = function(model,
 svc.fit = svm(Purchase~., data=OJ[train_inds,], kernel="linear", cost=0.01)
 summary(svc.fit)
 # The svc.fit model is a classification one, since $Purchase was stored as a factor.
-# A radial kernal was used using the parameter cost=0.01.
+# A linear kernal was used using the parameter cost=0.01.
 # In this case, the number of support vectors was 613, 308 in one class and 305 in another
 
 ##########################
@@ -40,7 +40,7 @@ pred_results(svc.fit)
 ##########################
 #### SECTION 1 Part d ####
 ##########################
-tune.lin = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="linear", ranges=list(cost=c(0.10, 10)))
+tune.lin = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="linear", ranges=list(cost=seq(0.01, 10, .5)))
 
 ##########################
 #### SECTION 1 Part e ####
@@ -50,19 +50,21 @@ pred_results(tune.lin$best.model)
 ##########################
 #### SECTION 1 Part f ####
 ##########################
-tune.rad = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="radial", ranges=list(cost=c(0.10, 10)))
+tune.rad = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="radial")
 pred_results(tune.rad$best.model)
 
 ##########################
 #### SECTION 1 Part g ####
 ##########################
-tune.poly = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="polynomial", degree=2, ranges=list(cost=c(0.10, 10)))
+tune.poly = tune(svm, Purchase ~ ., data=OJ[train_inds,], kernel="polynomial", degree=2)
 pred_results(tune.poly$best.model)
 
 ##########################
 #### SECTION 1 Part h ####
 ##########################
-# Overall, the Linear Support Vector Classification Model performed the best due to the lowest test error rate
+# Overall, the Support Vector Classification Model using a radial kernal performed the best due to the lowest test error rate
+# of 0.039252 but using a Linear kernal provided a near second result with an error rate of 0.04299.
+# ONe thing to note is that our models performed better with the test set when looking at error rates.
 
 ##########################
 #### SECTION 2 Part a ####
@@ -99,36 +101,45 @@ labels<-c(rep(1,n),rep(2,n),rep(3,n)) # the "true" labels of the points
 #### SECTION 2 Part b ####
 ##########################
 pca = prcomp(X)
-# not sure about the color of the plot
-biplot(pca, col=rainbow(2))
+plot(pca$x[,1], pca$x[,2], col=(labels+11), pch = 20)
 
 ##########################
 #### SECTION 2 Part c ####
 ##########################
 km.out = kmeans(X, centers=3)
-# The km.out result shows that there are 150 cluster means
+table(labels, km.out$cluster)
+# The km.out where K=3 result shows that all of the observations were perfectly assigned to the three clusters.
 
 ##########################
 #### SECTION 2 Part d ####
 ##########################
 km.out = kmeans(X, centers=2)
-# The km.out result shows that there are 100 cluster means
+table(labels, km.out$cluster)
+# The km.out when K=2 result shows that most of the results are assigned to one cluster.
+# It seems like one of the results from the previous model are now assigned to a different cluster.
 
 ##########################
 #### SECTION 2 Part e ####
 ##########################
 km.out = kmeans(X, centers=4)
-# The km.out result shows that there are 200 cluster means
+table(labels, km.out$cluster)
+# The km.out when K=4 shows that two clusters have a majority of the results. 
+# With an addition of a 4th cluster, it seems like the results when K=3 were split into an additional cluster
 
 ##########################
 #### SECTION 2 Part f ####
 ##########################
 km.out = kmeans(pca$x[,1:2], centers=3)
-# The km.out result shows that there are 150 cluster means
+table(labels, km.out$cluster)
+# The km.out using the PCA results show that the results are assigned similarly to using the raw data.
+# The results are perfectly assigned.
 
 ##########################
 #### SECTION 2 Part g ####
 ##########################
+#X.scaled.nocenter = scale(X, center=FALSE)
 X.scaled = scale(X)
 km.out = kmeans(X.scaled, centers=3)
-# The km.out result shows that there are 150 cluster means
+table(labels, km.out$cluster)
+# The km.out using the scaled and center data shows that the results are similar to the results from part b
+# However, scaling and not centering the data set produce results that are worse than scaling and centering.
